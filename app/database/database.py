10ce5746f436d0daa6c4758a74fca035f3042562
Diego Santos <diego.vsantos20018@gmail.com>
2026-08-24 15:10:43 -0300

    refactor: centralize database configuration

--- app/database/database.py
+++ app/database/database.py
@@ -1,11 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
+from app.config.settings import DATABASE_URL
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 engine = create_engine(DATABASE_URL)
 
